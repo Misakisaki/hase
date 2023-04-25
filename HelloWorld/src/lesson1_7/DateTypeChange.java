62bc6d4ab1c5ea01e1613bb91830be9bcd64cc5d
misakihase <misakihase@localhost>
2023-04-25 20:55:50 +0900

    Java 1_7 再提出

--- HelloWorld/src/lesson1_7/DateTypeChange.java
+++ HelloWorld/src/lesson1_7/DateTypeChange.java
@@ -30,6 +30,7 @@ public class DateTypeChange {
 
 		//			問３）
 		//			①double型の変数を定義し、「2 + 6.0」を代入して下さい。
+		
 		//			　double型の変数をコンソールに出力して下さい。
 		//			②int型の変数を定義し、double型の変数をint型に型変換して代入下さい。
 		//			　int型の変数をコンソールに出力して下さい。
@@ -37,23 +38,20 @@ public class DateTypeChange {
 		//			　String型の変数をコンソールに出力して下さい。
 		//			④「②」で定義したint型変数に、String型の変数をint型に型変換して代入して下さい。
 		//			　int型の変数をコンソールに出力して下さい。
-		double diver;
-		diver = 2 + 6.0;
-		System.out.println(diver);
-
-		double a = 1.0;
-		// double型からint型への変換
+		
+		//①double型を変数定義して、コンソールへ出力
+		double a= 2 + 6.0;
+		System.out.println(a);
+		//②int型を変数定義して、double型の変数をint型に型変換して、コンソールへ出力
 		int c = (int) a;
 		System.out.println(c);
-
-		int val = 123;
-		String val_str = String.valueOf(val);
-		System.out.println(val_str);
-
+		//③String型の変数を定義して、int型の変数をString型に型変換して、コンソールへ出力
+		String e = String.valueOf(c);
+		System.out.println(e);
+		//④int c をString型の変数をint型に型変換して、コンソールへ出力
+		c = Integer.parseInt(e);
+		System.out.println(c);
 		
-		int f = Integer.parseInt(val_str);
-		System.out.println(f);
-		
-
+	
 	}
 }
