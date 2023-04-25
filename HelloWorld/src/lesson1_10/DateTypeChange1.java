cf643aa3fb4817dd50525f6e28db091453cb5bb7
misakihase <misakihase@localhost>
2023-04-25 22:17:21 +0900

    Java 1_10 提出

---
+++ HelloWorld/src/lesson1_10/DateTypeChange1.java
@@ -0,0 +1,35 @@
+package lesson1_10;
+
+public class DateTypeChange1 {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		for(int a = 1; a <= 10; a++)
+		{
+		i+=2;
+		System.out.println(i);
+		}
+		
+	
+		// TODO 自動生成されたメソッド・スタブ
+		/*
+		問題.for文を使用し、以下のプログラムを作成してください。
+			①変数iに1を代入
+			
+			
+			
+			②変数iに2ずつ足す処理を10回繰り返す。
+				出力結果：  3
+							5
+							7
+							9
+							・
+							・
+							・
+							21
+							*/
+	
+	}
+
+}
