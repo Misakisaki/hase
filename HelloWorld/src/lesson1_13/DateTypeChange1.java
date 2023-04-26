eb16ed620babe64b0f61059fa5200bbee66d5c61
misakihase <misakihase@localhost>
2023-04-26 23:39:52 +0900

    Java_1_13 課題

--- HelloWorld/src/lesson1_13/DateTypeChange1.java
+++ HelloWorld/src/lesson1_13/DateTypeChange1.java
@@ -1,9 +1,5 @@
 package lesson1_13;
-
 public class DateTypeChange1 {
-	
-	
-
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		/*
@@ -17,13 +13,12 @@ public class DateTypeChange1 {
 				  3
 				  4
 				  5
-	*/
-			int[] a = {1, 2, 3, 4, 5};
-	 
-			System.out.println(a[0]);
-			System.out.println(a[1]);
-			System.out.println(a[2]);
-			System.out.println(a[3]);
-			System.out.println(a[4]);
+		 */
+		// 1~5を入れるための配列宣言
+		int[] a = {1, 2, 3, 4, 5};
+		// 配列aから順番に数字を表示する処理
+		for(int i = 0;  i <= 4; i++){
+			System.out.println(a[i]);
 		}
+	}
 }
