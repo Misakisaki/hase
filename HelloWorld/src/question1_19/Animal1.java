235924dce6e853230ce128028d891721e2cab605
misakihase <misakihase@localhost>
2023-05-08 22:15:16 +0900

    1_19 Java 提出

---
+++ HelloWorld/src/question1_19/Animal1.java
@@ -0,0 +1,22 @@
+package question1_19;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+public class Animal1 {
+	public static void main(String[] args) {
+		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
+		// Dogクラスをインスタンス化する
+		Dog bbb = new Dog();
+		System.out.println(bbb.AnimalName);
+
+		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
+		Dog aaa = new Dog(6);
+		System.out.println(aaa.AnimalMath);
+		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
+		//Calendarクラスのオブジェクトを生成する
+		Calendar cl = Calendar.getInstance();
+
+		//SimpleDateFormatクラスでフォーマットパターンを設定する
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd H:m:s");
+		System.out.println(sdf.format(cl.getTime()));
+	}
+}
--- HelloWorld/src/question1_19/Dog.java
+++ HelloWorld/src/question1_19/Dog.java
@@ -1,23 +1,17 @@
 package question1_19;
-
 public class Dog {
-		// Q1：フィールドに動物の名前の変数を定義してください。
-		
-		// Q2：フィールドに動物の数の変数を定義してください。
-		
-		// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
-		
-		// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
-	
-
-	 
-	public class Animal {
-		public void publicMethod() {
-			System.out.println("publicMethodから出力しています。");
-	
-		
-	
-		}
+	// Q1：フィールドに動物の名前の変数を定義してください。
+	// 変数の定義
+	String AnimalName;
+	// Q2：フィールドに動物の数の変数を定義してください。
+	int AnimalMath;
+	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
+	//コンストラクタに関する記述 
+	public Dog(){ 
+		this.AnimalName = "犬";
 	}
-
-// Java インポート　
+	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+	public Dog(int head){
+		AnimalMath = head;
+	}
+}
