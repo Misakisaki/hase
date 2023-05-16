fdfcd48a6ce2c6eaad1a8a3411a3e820d5f4f63d
misakihase <misakihase@10.16.35.209>
2023-05-16 23:13:15 +0900

    java 1_19 提出

--- HelloWorld/src/question1_19/Animal1.java
+++ HelloWorld/src/question1_19/Animal1.java
@@ -7,14 +7,12 @@ public class Animal1 {
 		// Dogクラスをインスタンス化する
 		Dog bbb = new Dog();
 		System.out.println(bbb.AnimalName);
-
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
 		Dog aaa = new Dog(6);
 		System.out.println(aaa.AnimalMath);
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
 		//Calendarクラスのオブジェクトを生成する
 		Calendar cl = Calendar.getInstance();
-
 		//SimpleDateFormatクラスでフォーマットパターンを設定する
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd H:m:s");
 		System.out.println(sdf.format(cl.getTime()));
--- HelloWorld/src/question1_19/Dog.java
+++ HelloWorld/src/question1_19/Dog.java
@@ -1,4 +1,5 @@
 package question1_19;
+
 public class Dog {
 	// Q1：フィールドに動物の名前の変数を定義してください。
 	// 変数の定義
@@ -7,11 +8,11 @@ public class Dog {
 	int AnimalMath;
 	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
 	//コンストラクタに関する記述 
-	public Dog(){ 
+	public Dog() {
 		this.AnimalName = "犬";
 	}
 	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
-	public Dog(int head){
+	public Dog(int head) {
 		AnimalMath = head;
 	}
 }
