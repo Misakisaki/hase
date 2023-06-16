65675eca9c5dc1502488a36cea1f5cfafd50ed3b
misakihase <misakihase@localhost>
2023-06-16 11:15:27 +0900

    Qes6 Java 再提出

--- HelloWorld/src/curriculum_B/Qes6.java
+++ HelloWorld/src/curriculum_B/Qes6.java
@@ -1,76 +1,73 @@
 package curriculum_B;
 
 import java.util.Random;
-// scannerをインポートする
+// スキャナーをインポートする
 import java.util.Scanner;
 
 public class Qes6 {
-	//  入力した商品の残り台数が出力されるシステムを下記の条件で作成してください
-	// ・拡張for文・Switch文・条件演算子を使用すること　※普通のif文は使用不可
-	public static void main(String[] args) {
-		// 「パソコン」「冷蔵庫」「扇風機」「洗濯機」「加湿器」「テレビ」「ディスプレイ」「その他商品」を代入
-		// scannerを初期化
-		Scanner scanner = new Scanner(System.in);
-		// checkに入力した文字を受け取れるようにする　	// カンマを代入
-		String[] checks = scanner.nextLine().split("、");
-		// Randomを初期化する
-		Random rand = new Random();
 
-		// scannerを閉じる
-		//	 scanner.close();
+	public static void main(String[] args) {
+		// スキャナークラスを生成
+		Scanner scanner = new Scanner(System.in);
+		// 変数に入力した製品名にカンマを代入して受け取れるようにする
+		String[] checks = scanner.nextLine().split("、");
+		// ランダムクラスを生成
+		Random rand = new Random();
 
 		// 製品名を記入した際に在庫数が出力される（0〜11台）
 		for (String check : checks) {
-			
-			// switch文にcheckを挿入
+
+			// 繰り返し文に製品名を挿入
 			switch (check) {
-			// itemがパソコンだった時のcaseを作成
+			// 製品名がパソコンが入力された場合
 			case "パソコン":
-				// 「パソコンの残り台数はrand台です」と出力
-				System.out.println("パソコンの残り台数は" + rand.nextInt(11) + "台です");
+				// ランダムクラスを使用し、パソコンの残り台数をランダムな値でコンソールにて出力
+				System.out.println("パソコンの残り台数は" + rand.nextInt(11) + 1 + "台です");
 				break;
-			// itemが冷蔵庫だった時のcaseを作成
+			// 製品名が冷蔵庫が入力された場合
 			case "冷蔵庫":
-				// 「冷蔵庫の残り台数はrand台です」と出力
-				System.out.println("冷蔵庫の残り台数は" + rand.nextInt(11) + "台です");
+				// ランダムクラスを使用し、冷蔵庫の残り台数をランダムな値でコンソールにて出力
+				System.out.println("冷蔵庫の残り台数は" + rand.nextInt(11) + 1 + "台です");
 				break;
 
-			// itemが扇風機だった時のcaseを作成
+			// 製品名が扇風機が入力された場合
 			case "扇風機":
-				// 「扇風機の残り台数はrand台です」と出力
-				System.out.println("扇風機の残り台数は" + rand.nextInt(11) + "台です");
+				// ランダムクラスを使用し、扇風機の残り台数をランダムな値でコンソールにて出力
+				System.out.println("扇風機の残り台数は" + rand.nextInt(11) + 1 + "台です");
 				break;
 
-			// itemが洗濯機だった時のcaseを作成
+			// 製品名が洗濯機が入力された場合
 			case "洗濯機":
-				// 「洗濯機の残り台数はrand台です」と出力
-				System.out.println("洗濯機の残り台数は" + rand.nextInt(11) + "台です");
+				// ランダムクラスを使用し、洗濯機の残り台数をランダムな値でコンソールにて出力
+				System.out.println("洗濯機の残り台数は" + rand.nextInt(11) + 1 + "台です");
 				break;
 
-			// itemが加湿器が入力された場合
+			// 製品名が加湿器が入力された場合
 			case "加湿器":
-				// 「加湿器の残り台数はrand台です」と出力
-				System.out.println("加湿器の残り台数は" + rand.nextInt(11) + "台です");
+				// ランダムクラスを使用し、加湿器の残り台数をランダムな値でコンソールにて出力
+				System.out.println("加湿器の残り台数は" + rand.nextInt(11) + 1 + "台です");
 				break;
 
-			// itemがテレビが入力された場合
+			// 製品名がテレビが入力された場合
 			case "テレビ":
-				// itemがディスプレイが入力された場合
+				// 製品名がディスプレイが入力された場合
 			case "ディスプレイ":
-				// テレビのランダム数をTV_randに代入
-				int TV_rand = rand.nextInt(11);
-				// Switch文内でテレビとディスプレイは続けて書くようにしてください、条件演算子で出力される値を変更してください
-				int result = check.equals("テレビ") ? TV_rand : 11 - TV_rand;	 
-				// テレビもしくはディスプレイの残り台数はresult台ですと出力
+				// テレビの残り台数をランダムな台数に代入
+				int tv_rand = rand.nextInt(11) + 1;
+				// 条件演算子を使いテレビとディプレイのうちテレビですか？という条件
+				int result = check.equals("テレビ") ? tv_rand : (11) + 1 - tv_rand;
+				// テレビもしくはディスプレイの残り台数を出力
 				System.out.println(check + "の残り台数は" + result + "台です");
-				// switch文を中断する
+				// 繰り返し処理を中断する
 				break;
 
 			// いずれの場合でもなかった時の処理を作成
 			default:
-				// itemは指定の商品ではありませんを出力
-				System.out.println("『 その他商品 』は指定の商品ではありません");
-				
+
+				// 条件に含まない家電は「指定の商品ではありません」を出力
+				System.out.println("『" + check + "』は指定の商品ではありません");
+				// スキャナーを閉じる
+				scanner.close();
 				break;
 			}
 		}
