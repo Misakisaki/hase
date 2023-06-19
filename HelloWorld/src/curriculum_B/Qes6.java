a17ef12b95f57e364bd04f214e907f52027c0f4b
misakihase <misakihase@localhost>
2023-06-19 20:00:53 +0900

    java 6 再再提出

--- HelloWorld/src/curriculum_B/Qes6.java
+++ HelloWorld/src/curriculum_B/Qes6.java
@@ -14,6 +14,9 @@ public class Qes6 {
 		// ランダムクラスを生成
 		Random rand = new Random();
 
+		// テレビの残り台数をランダムな台数に代入
+		int tv_rand = rand.nextInt(12);
+
 		// 製品名を記入した際に在庫数が出力される（0〜11台）
 		for (String check : checks) {
 
@@ -22,40 +25,39 @@ public class Qes6 {
 			// 製品名がパソコンが入力された場合
 			case "パソコン":
 				// ランダムクラスを使用し、パソコンの残り台数をランダムな値でコンソールにて出力
-				System.out.println("パソコンの残り台数は" + rand.nextInt(11) + 1 + "台です");
+				System.out.println("パソコンの残り台数は" + rand.nextInt(12) + "台です");
 				break;
 			// 製品名が冷蔵庫が入力された場合
 			case "冷蔵庫":
 				// ランダムクラスを使用し、冷蔵庫の残り台数をランダムな値でコンソールにて出力
-				System.out.println("冷蔵庫の残り台数は" + rand.nextInt(11) + 1 + "台です");
+				System.out.println("冷蔵庫の残り台数は" + rand.nextInt(12) + "台です");
 				break;
 
 			// 製品名が扇風機が入力された場合
 			case "扇風機":
 				// ランダムクラスを使用し、扇風機の残り台数をランダムな値でコンソールにて出力
-				System.out.println("扇風機の残り台数は" + rand.nextInt(11) + 1 + "台です");
+				System.out.println("扇風機の残り台数は" + rand.nextInt(12) + "台です");
 				break;
 
 			// 製品名が洗濯機が入力された場合
 			case "洗濯機":
 				// ランダムクラスを使用し、洗濯機の残り台数をランダムな値でコンソールにて出力
-				System.out.println("洗濯機の残り台数は" + rand.nextInt(11) + 1 + "台です");
+				System.out.println("洗濯機の残り台数は" + rand.nextInt(12) + "台です");
 				break;
 
 			// 製品名が加湿器が入力された場合
 			case "加湿器":
 				// ランダムクラスを使用し、加湿器の残り台数をランダムな値でコンソールにて出力
-				System.out.println("加湿器の残り台数は" + rand.nextInt(11) + 1 + "台です");
+				System.out.println("加湿器の残り台数は" + rand.nextInt(12) + "台です");
 				break;
 
 			// 製品名がテレビが入力された場合
 			case "テレビ":
 				// 製品名がディスプレイが入力された場合
 			case "ディスプレイ":
-				// テレビの残り台数をランダムな台数に代入
-				int tv_rand = rand.nextInt(11) + 1;
+
 				// 条件演算子を使いテレビとディプレイのうちテレビですか？という条件
-				int result = check.equals("テレビ") ? tv_rand : (11) + 1 - tv_rand;
+				int result = check.equals("テレビ") ? tv_rand : 11 - tv_rand;
 				// テレビもしくはディスプレイの残り台数を出力
 				System.out.println(check + "の残り台数は" + result + "台です");
 				// 繰り返し処理を中断する
@@ -66,10 +68,11 @@ public class Qes6 {
 
 				// 条件に含まない家電は「指定の商品ではありません」を出力
 				System.out.println("『" + check + "』は指定の商品ではありません");
-				// スキャナーを閉じる
-				scanner.close();
+
 				break;
 			}
 		}
+		// スキャナーを閉じる
+		scanner.close();
 	}
 }
