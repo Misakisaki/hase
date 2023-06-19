6db413d3337e776c760c95a6fe19c70dd1f5c49c
misakihase <misakihase@localhost>
2023-06-19 19:58:30 +0900

    java 4 再再提出

--- HelloWorld/src/curriculum_B/Qes4.java
+++ HelloWorld/src/curriculum_B/Qes4.java
@@ -1,27 +1,28 @@
 package curriculum_B;
 
-import java.util.StringJoiner;
-
 public class Qes4 {
 	public static void main(String[] args) {
-		// 課題４
-		// 1~9の入った変数の定義
-		int number[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-		// 変数0の定義、１ずつ足していく処理
-		for (int i = 0; i < number.length; i++) {
-			// 変数0の定義、１ずつ足していく処理
-			for (int j = 0; j < number.length; j++) {
+		// 課題5
+		// 1~20の入った変数の定義
+
+		// 変数0の定義、１ずつ足していく処理　1の段9の段まで表示
+		for (int i = 1; i < 10; i++) {
+			// 変数0の定義、１ずつ足していく処理 10の段から20の段まで表示
+			for (int j = 1; j < 10; j++) {
 				// 掛け算の式
-				int answer = number[i] * number[j];
-				// StringJoinerを使用した文字列の連結方法
-				StringJoiner sj = new StringJoiner(" ", " ", " ||");
-				sj.add(String.format("%02d", number[i])).add("*").add(String.format("%02d", number[j]))
-						.add("=").add(String.format("%02d", answer));
-				// 掛け算の結果出力
-				System.out.print(sj);
+				int answer = i * j;
+
+				// 左辺が9の段までは2桁で0埋め　左辺が10の段以降は3桁で0埋めする
+				if (j >= 9) {
+
+					System.out.printf("%02d * %02d = %02d %n", j, i, answer);
+
+					// それ以外は2桁で0埋め、答えは3桁で0埋めする
+				} else {
+					System.out.printf("%02d * %02d = %02d || ", j, i, answer);
+
+				}
 			}
-			// 改行
-			System.out.println();
 		}
 	}
 }
