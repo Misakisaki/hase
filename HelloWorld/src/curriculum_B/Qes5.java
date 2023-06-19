0e3ae78774a3a5dd1aa48272fb1d7e777da1697e
misakihase <misakihase@localhost>
2023-06-19 19:59:50 +0900

    Java　5　再再提出

--- HelloWorld/src/curriculum_B/Qes5.java
+++ HelloWorld/src/curriculum_B/Qes5.java
@@ -1,7 +1,5 @@
 package curriculum_B;
 
-import java.util.StringJoiner;
-
 public class Qes5 {
 	public static void main(String[] args) {
 		// 課題5
@@ -14,22 +12,16 @@ public class Qes5 {
 				// 掛け算の式
 				int answer = i * j;
 
-				// StringJoinerを使用した文字列の連結方法
-				StringJoiner sj = new StringJoiner(" ", " ", " ||");
 				// 左辺が9の段までは2桁で0埋め　左辺が10の段以降は3桁で0埋めする
-				if (j > 9) {
-					sj.add(String.format("%03d", j)).add("*").add(String.format("%02d", i))
-							.add("=").add(String.format("%03d", answer));
+				if (j >= 20) {
+
+					System.out.printf("%03d * %03d = %03d %n", j, i, answer);
 
 					// それ以外は2桁で0埋め、答えは3桁で0埋めする
 				} else {
-					sj.add(String.format("%02d", j)).add("*").add(String.format("%02d", i))
-							.add("=").add(String.format("%03d", answer));
+					System.out.printf("%03d * %03d = %03d || ", j, i, answer);
+
 				}
-				// 掛け算の結果出力
-				System.out.print(sj);
-				// 改行
-				System.out.println();
 			}
 		}
 	}
