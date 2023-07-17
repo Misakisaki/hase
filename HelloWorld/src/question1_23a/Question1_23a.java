debfe558e85604c6ee86e7a4eb14b81f7549e656
misakihase <misakihase@localhost>
2023-07-17 22:14:29 +0900

    java 23 再提出

--- HelloWorld/src/question1_23a/Question1_23a.java
+++ HelloWorld/src/question1_23a/Question1_23a.java
@@ -6,26 +6,22 @@ public class Question1_23a {
 
 	public static void main(String[] args) {
 
-		// Sushiクラスをインスタンス化する
+		// Question1_23bクラスをインスタンス化する
 		Question1_23b Question1_23b = new Question1_23b();
-
 		// セッターを呼び出す
 		// 動物名：ライオン
-		Question1_23b.setname("ライオン");
+		Question1_23b.setname1("ライオン");
 		// 体長：2.1m
 		Question1_23b.setBodylength("2.1");
 		// 速度：80km/h
 		Question1_23b.setSpeed("80");
-
 		// ゲッターを呼び出して変数に代入し、各内容をコンソールに出力してください。
 		System.out.println(Question1_23b);
 	}
 }
 /*
-	
 	下記がコンソールに出力されるように作成してください
 	※thisとsetterとgetterとフィールドを使ってください
-	
 	動物名：ライオン
 	体長：2.1m
 	速度：80km/h
--- HelloWorld/src/question1_23b/Question1_23b.java
+++ HelloWorld/src/question1_23b/Question1_23b.java
@@ -1,42 +1,53 @@
 package question1_23b;
+
 public class Question1_23b {
+	// name変数定義
+	private String name;
+	// bodylength変数定義
+	private String bodylength;
+	// speed変数定義
+	private String speed;
 
-	private String name = "ライオン";
-	private String bodylength = "2.1";
-	private String speed = "80";
-
+	// getterとは、setterでセットしたフィールドのnameの値を取得して、returnで呼び出し元に値を返すメソッドのことを指しています。
 	public String getname() {
 		return name;
 	}
 
-	public void setライオン(String name) {
+	// setterは呼び出し元のメソッドから値を引数で受け取り、引数のnameの値をthisを使ってフィールドにセットしています
+	public void setname1(String name) {
 		this.name = name;
 	}
 
+	// getterとは、setterでセットしたフィールドのBodylengthの値を取得して、returnで呼び出し元に値を返すメソッドのことを指しています。
 	public String getBodylength() {
 		return bodylength;
 	}
 
+	// setterは呼び出し元のメソッドからBodylengthの値を引数で受け取り、引数の値をthisを使ってフィールドにセットしています
 	public void setBodylength(String bodylength) {
 		this.bodylength = bodylength;
 	}
 
+	// getterとは、setterでセットしたフィールドのSpeedの値を取得して、returnで呼び出し元に値を返すメソッドのことを指しています。
 	public String getSpeed() {
 		return getSpeed();
 	}
 
+	// setterは呼び出し元のメソッドからSpeedの値を引数で受け取り、引数の値をthisを使ってフィールドにセットしています
 	public void setSpeed(String speed) {
 		this.speed = speed;
 	}
-	
+
+	// returnで呼び出し元に代入された値を返し出力するメソッド
 	public String toString() {
 		return "動物名：" + name + "\n" + "体長：" + bodylength + "m" + "\n速度：" + speed + "km/h";
 	}
 
 	public void setname(String string) {
 		// TODO 自動生成されたメソッド・スタブ
-		
-	}}
+
+	}
+}
 
 /*
 
